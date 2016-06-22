9e1b9c135f71901e827a1d55255f9e4cdf1f9213
Alexander Rössler <mail.aroessler@gmail.com>
2016-06-22 16:00:39 +0200

    use protobuf and QML mode

--- config/main.el
+++ config/main.el
@@ -589,6 +589,14 @@
 (use-package scad-mode
   :ensure t)
 
+;; Protobuf
+(use-package protobuf-mode
+  :ensure t)
+
+;; QML
+(use-package qml-mode
+  :ensure t)
+
 ;; XML formatting
 (defun bf-pretty-print-xml-region (begin end)
   "Pretty format XML markup in region. You need to have nxml-mode
