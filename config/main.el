85a28baef7fe68cf7137a644468c192bcfe86a56
Alexander Rössler <alexander.roessler@zuehlke.com>
2017-04-24 19:22:31 +0200

    adding Discover mode

--- config/main.el
+++ config/main.el
@@ -643,3 +643,25 @@ by using nxml's indentation rules."
 ;; vala mode
 (use-package vala-mode
   :ensure t)
+
+(use-package discover
+  :ensure t
+  :config
+  (discover-add-context-menu
+   :context-menu '(isearch
+                   (description "Isearch, occur and highlighting")
+                   (lisp-switches
+                    ("-cf" "Case should fold search" case-fold-search t nil))
+                   (lisp-arguments
+                    ("=l" "context lines to show (occur)"
+                     "list-matching-lines-default-context-lines"
+                     (lambda (dummy) ((interactive "P")) (read-number "Number of context lines to show: "))))
+                   (actions
+                    ("Isearch"
+                     ("_" "isearch forward symbol" isearch-forward-symbol)
+                     ("w" "isearch forward word" isearch-forward-word))
+                    ("Occur"
+                     ("o" "occur" occur))
+                    ("More"
+                     ("h" "highlighters ..." makey-key-mode-popup-isearch-highlight))))
+   :bind "M-s"))
